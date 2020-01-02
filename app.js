144b9b38f4010d84501bd4a1dbe4d70f2075eac9
Elena Man <nicoletaman97@gmail.com>
2020-01-02 15:01:33 +0200

    created react components for the Player, Counter and included all the existing component in the App component

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Header() {
+const Header = () => {
     return (
         <header>
             <h1>Scoreboard</h1>
@@ -7,10 +7,40 @@ function Header() {
     );
 }
 
+const Player = () => {
+    return (
+        <div className='player'>
+            <span className='player-name'>
+                Name here
+            </span>
+            <Counter />
+        </div>
+    );
+}
 
+const Counter = () => {
+    return (
+        <div className='counter'>
+            <button className='counter-action decrement'> - </button>
+            <span className='counter-score'>0</span>
+            <button className='counter-action increment'> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className='scoreboard'>
+            <Header />
+
+            {/* {Players list} */}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <App />,
     //container element
     document.getElementById('root')
 );
