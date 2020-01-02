faf1e8ee6124348f399b999eb5b595fa807b73cb
Elena Man <nicoletaman97@gmail.com>
2020-01-02 14:48:12 +0200

    created react components as functions

---
+++ app.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className='stats'>Players: 1</span>
+        </header>
+    );
+}
+
+
+
+ReactDOM.render(
+    header,
+    //container element
+    document.getElementById('root')
+);
